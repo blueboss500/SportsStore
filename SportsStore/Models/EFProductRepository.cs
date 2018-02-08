f8c3f8923b9d5e5effc1530b366dd7a2e055007a
Blue <blued2@medtronic.com>
2018-02-07 20:28:58 -0700

    Add project files.

---
+++ SportsStore/Models/EFProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models
+{
+    public class EFProductRepository : IProductRepository
+    {
+        //member vars
+        private ApplicationDbContext mContext;
+
+        //constructor
+        public EFProductRepository(ApplicationDbContext ctx)
+        {
+            mContext = ctx;
+        }
+
+        public IQueryable<Product> Products => mContext.Products;
+    }
+}
